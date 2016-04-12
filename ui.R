4b17425db15ddafd03328ba9488ace9f8c32436b
ciaranporter5 <ciaran.porter5@mail.dcu.ie>
2016-04-13 00:31:17 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,23 +5,24 @@ shinyServer(
     
     fluidRow(
       column(3,
-             selectizeInput("name", "Name",c("Charlie", "Mary")),
+             selectizeInput("name", "Name",c("Darren", "Seamus", "John", "Richard", "Robbie",
+                                             "Glenn", "Jeff", "Wes", "James", 
+                                             "Shane", "Jonathan")),
              checkboxInput('male', 'Male'),
              checkboxInput('female', 'Female')
       ),
-      
       column(2,
-             selectizeInput("state", "State",c("CA", "NY"))
+             selectizeInput("state", "State",c("CA", "NY", "NJ", "MA"))
       ),
-      
       column(7,
              sliderInput("year", "Year",min = 1880, max = 2020, step = 1, sep ="",
                          value = c(1880, 2014), format ="$####")
       )
     ),
-  
+    
     mainPanel(
-     plotOutput("Neutrality")
+     plotOutput("Neutrality"),
+     plotOutput("LineGraph")
     )
   )
 )
