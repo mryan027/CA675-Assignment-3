0c32d13766ff5ec67f5a126ebf4328b58ea34395
ciaranporter5 <ciaran.porter5@mail.dcu.ie>
2016-04-13 23:44:32 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,14 +5,13 @@ shinyServer(
     
     fluidRow(
       column(3,
-             selectizeInput("name", "Name",c("Darren", "Seamus", "John", "Richard", "Robbie",
-                                             "Glenn", "Jeff", "Wes", "James", 
-                                             "Shane", "Jonathan")),
+             selectizeInput("name", "Name", as.character(UniqueNames$Name[c(1:30000)])),
              checkboxInput('male', 'Male'),
              checkboxInput('female', 'Female')
+             # 40k names -> becomes quite slow to load
       ),
       column(2,
-             selectizeInput("state", "State",c("CA", "NY", "NJ", "MA"))
+             selectizeInput("state", "State",as.character(StateList$State))
       ),
       column(7,
              sliderInput("year", "Year",min = 1880, max = 2020, step = 1, sep ="",
@@ -21,8 +20,17 @@ shinyServer(
     ),
     
     mainPanel(
-     plotOutput("Neutrality"),
-     plotOutput("LineGraph")
+      tabsetPanel(
+        tabPanel("Gender Mix", plotOutput("Neutrality")),
+        tabPanel("Historic", plotOutput("LineGraph")),
+        tabPanel("Top 10 Names"),
+        tabPanel("Bottom 10 Names"),
+        tabPanel("Top 5 States"),
+        tabPanel("Top 5 Years"),
+        tabPanel("Wiki"),
+        tabPanel("Map")
+        # include other tabs - wiki etc in here
+      )
     )
   )
 )
