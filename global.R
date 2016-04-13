017ef4bc827d3e03268edd3aeb9592d154be1ef1
ciaranporter5 <ciaran.porter5@mail.dcu.ie>
2016-04-13 23:42:57 +0100

    Update global.R

--- global.R
+++ global.R
@@ -1,2 +1,4 @@
 # read-in csv file of all names
 CombinedNames<- read.csv("CombinedNames.csv", header = TRUE)
+UniqueNames <- read.csv("UniqueNames.csv", header = TRUE)
+StateList <- read.csv("StateList.csv", header = TRUE)
