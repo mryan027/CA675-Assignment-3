a5cf4ed60d1e6c7a7237a86cc7a4760773ae3065
ciaranporter5 <ciaran.porter5@mail.dcu.ie>
2016-04-15 00:01:23 +0100

    Update global.R

--- global.R
+++ global.R
@@ -1,4 +1,6 @@
 # read-in csv file of all names
+#install.packages('DT')
+library('DT')
 CombinedNames<- read.csv("CombinedNames.csv", header = TRUE)
 UniqueNames <- read.csv("UniqueNames.csv", header = TRUE)
 StateList <- read.csv("StateList.csv", header = TRUE)
