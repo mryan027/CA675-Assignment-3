652fd70d83b0b3302fcf1f367153cfc4ecc88c0f
ciaranporter5 <ciaran.porter5@mail.dcu.ie>
2016-04-17 00:47:00 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,16 +5,12 @@ shinyServer(
     
     fluidRow(
       column(3,
-             #selectizeInput("name", "Name",c("Darren", "Seamus", "John", "Richard", "Robbie",
-              #                               "Glenn", "Jeff", "Wes", "James", 
-               #                              "Shane", "Jonathan")),
-             selectizeInput("name", "Name", as.character(UniqueNames$Name[c(1:100)])),
+             selectizeInput("name", "Name", choices = NULL),
              checkboxInput('male', 'Male'),
              checkboxInput('female', 'Female')
-             # 40k names -> becomes quite slow to load
       ),
       column(2,
-             selectizeInput("state", "State",as.character(StateList$State))
+             selectInput("state", "State",as.character(StateList$State),selectize = TRUE)
       ),
       column(7,
              sliderInput("year", "Year",min = 1880, max = 2020, step = 1, sep ="",
@@ -26,13 +22,12 @@ shinyServer(
       tabsetPanel(
         tabPanel("Gender Mix", plotOutput("Neutrality")),
         tabPanel("Historic", plotOutput("LineGraph")),
-        tabPanel("Top 10 Names"),
-        tabPanel("Bottom 10 Names"),
-        tabPanel("Top 5 States"),
+        tabPanel("Top 10 Names",dataTableOutput("Top10Names")),
+        tabPanel("Bottom 10 Names",dataTableOutput("Bottom10Names")),
+        tabPanel("Top 5 States",dataTableOutput("Top5ByState")),
         tabPanel("Top 5 Years", dataTableOutput("Top5ByYear")),
         tabPanel("Wiki"),
-        tabPanel("Map")
-        # include other tabs - wiki etc in here
+        tabPanel("Map", plotOutput("MapPlot"))
       )
     )
   )
