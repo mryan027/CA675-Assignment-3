01064bcbd1a248469d27365de774dffb6297b680
ciaranporter5 <ciaran.porter5@mail.dcu.ie>
2016-04-17 20:23:25 +0100

    Update global.R

--- global.R
+++ global.R
@@ -1,14 +1,17 @@
 # install and load required packages
 #install.packages('DT')
+#install.packages('plyr')
 #install.packages('ggplot2')
 #install.packages('ggmap')
 #install.packages('openintro')
-library('DT')
+library(DT)
+library(plyr)
 library(ggplot2)
 library(ggmap)
 library(openintro)
 
-# read-in csv file of all names
+# read-in rds file of all names
 CombinedNames <- readRDS("CombinedNames.rds")
 UniqueNames <- readRDS("UniqueNames.rds")
 StateList <- readRDS("StateList.rds")
+WikiListClean <- readRDS("WikiList.rds")
