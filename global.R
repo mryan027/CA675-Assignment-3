356b2dbf160a68d88c3e043d758f7c7b6b6f5fe0
ciaranporter5 <ciaran.porter5@mail.dcu.ie>
2016-04-20 23:04:55 +0100

    Update global.R

--- global.R
+++ global.R
@@ -10,8 +10,16 @@ library(ggplot2)
 library(ggmap)
 library(openintro)
 
+#CombinedNames <- read.csv("CombinedNames.csv", header = TRUE)
+#UniqueNames <- read.csv("UniqueNames.csv", header = TRUE)
+
+#saveRDS(CombinedNames,"CombinedNames.rds")
+#saveRDS(UniqueNames ,"UniqueNames.rds")
+
 # read-in rds file of all names
 CombinedNames <- readRDS("CombinedNames.rds")
 UniqueNames <- readRDS("UniqueNames.rds")
 StateList <- readRDS("StateList.rds")
 WikiListClean <- readRDS("WikiList.rds")
+
+
